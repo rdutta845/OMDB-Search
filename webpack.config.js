eebdc0b94e220a3c7dfdd397eba65284415097ca
Rahul Dutta <rahul.dutta@olacabs.com>
2020-02-26 00:51:31 +0530

    init

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+
+let obj = {};
+obj.entry = './src/index.js';
+
+obj.module = {};
+
+obj.module.rules = [
+    {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"]
+    },
+    {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+    }
+]
+
+obj.output = {
+    path: path.join(__dirname, ',dist'),
+    filename: 'index.js'
+};
+obj.plugins = [
+    new htmlWebpackPlugin({
+        template: './src/index.html'
+    })
+];
+
+module.exports = obj;
