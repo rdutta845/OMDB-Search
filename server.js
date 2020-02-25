70d55a85b6fc8ea82972a6b4cef053a8dd0a55a5
Rahul Dutta <rahul.dutta@olacabs.com>
2020-02-26 02:28:54 +0530

    create build

--- server.js
+++ server.js
@@ -9,12 +9,7 @@ app.use("/scripts", express.static(__dirname + '/scripts'));
 
 // viewed at based directory http://localhost:8080/
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname + 'views/index.html'));
-});
-
-// add other routes below
-app.get('/about', function (req, res) {
-  res.sendFile(path.join(__dirname + 'views/about.html'));
+  res.sendFile(path.join(__dirname + 'dist/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
