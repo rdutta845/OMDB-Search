a521d41cff64631796d04c31764732078a9f3577
Rahul Dutta <rahul.dutta@olacabs.com>
2020-02-26 02:35:36 +0530

    create build

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ var app = express();
 var path = require('path');
 
 app.use(express.static(path.join(__dirname)));
-app.use("dist/index.js", express.static(__dirname));
+app.use("/dist/index.js", express.static(__dirname));
 
 
 // viewed at based directory http://localhost:8080/
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname + 'dist/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
